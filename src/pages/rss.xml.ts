fed6c30c1e4cfbb2a3ff0d9d5bc2d37ba68faef2
JKmoises <moi.prado@duocuc.cl>
2024-08-21 15:39:55 -0400

    Agregar contenido y imagen de blog a RSS Feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,29 +2,51 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
+
+
 export const GET: APIRoute = async ({ params, request, site }) => {
   const blogPosts = await getCollection("blog");
 
   return rss({
-    //* Ruta del archivo xsl para el estilo del rss 
+    //* Ruta del archivo xsl para el estilo del rss
     // stylesheet: "/styles/rss.xsl",
     //* `<title>` campo en el xml generado
     title: "Fernando Blog",
     //* `<description>` campo en el xml generado
     description: "Un simple blog de mis aventuras con astro",
+    xmlns: {
+      media: "http://search.yahoo.com/mrss/",
+    },
     //* Usa el "site" desde el contexto del endpoint
     // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: site ?? "",
     //* Array de `<item>`s en el xml generado
     //* Consulta la sección "Generando `items`" para ejemplos utilizando colecciones de contenido y glob imports
-    items: blogPosts.map(({ data, slug }) => ({
-        title: data.title,
-        pubDate: data.date,
-        description: data.description,
-        //* Ruta relativa a la raíz del sitio de posts escritos en markdown
-        link: `posts/${slug}`,
+    items: blogPosts.map(({ data, slug, body }) => ({
+      title: data.title,
+      pubDate: data.date,
+      description: data.description,
+      //* Ruta relativa a la raíz del sitio de posts escritos en markdown
+      link: `posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+
+      customData: `<media:content
+        type="image/${data.image.format === "jpg" ? "jpeg" : "png"}"
+        width="${data.image.width}"
+        height="${data.image.height}"
+        medium="image"
+        url="${site + data.image.src}" />
+      `,
     })),
+
     //* (opcional) inyecta xml personalizado como el idioma del feed
     customData: `<language>es-mx</language>`,
- });
+  });
 };
